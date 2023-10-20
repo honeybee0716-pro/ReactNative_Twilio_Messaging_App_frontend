aea857d9254b029167d62186b030232404bc2a88
softangel0088 <softangel0088@gmail.com>
2023-10-20 14:02:56 +0200

    frontend

--- config/api.js
+++ config/api.js
@@ -2,5 +2,5 @@
 // export const API_URL = "http://149.154.158.249:5000/api";
 
 
-// export const API_URL = "http://149.154.158.249:5000/api";
-export const API_URL = "http://192.168.130.97:5000/api";
+export const API_URL = "http://149.154.158.249:5000/api";
+// export const API_URL = "http://192.168.130.97:5000/api";
