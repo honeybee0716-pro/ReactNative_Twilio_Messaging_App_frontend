de680b038be432061b62a33b3f2d444d8482c312
softangel0088 <softangel0088@gmail.com>
2023-10-20 15:16:20 +0200

    frontned

--- config/api.js
+++ config/api.js
@@ -2,5 +2,6 @@
 // export const API_URL = "http://149.154.158.249:5000/api";
 
 
-export const API_URL = "http://149.154.158.249:5000/api";
+// export const API_URL = "http://149.154.158.249:5000/api";
 // export const API_URL = "http://192.168.130.97:5000/api";
+export const API_URL = "https://chattestapp-28856047d52b.herokuapp.com/api";
